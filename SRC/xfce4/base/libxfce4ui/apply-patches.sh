73eb73797b72ec02ed953fe872879cbd00b83ca7
brokenman <brokenman@porteus.org>
2014-10-19 12:53:35 +0000

    Removed patch that is now in git source

--- SRC/xfce4/base/libxfce4ui/apply-patches.sh
+++ SRC/xfce4/base/libxfce4ui/apply-patches.sh
@@ -5,6 +5,6 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 # add more keyboard shortcuts to make multimedia keyboards work out of the box (Fedora)
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/libxfce4ui-4.10.0-keyboard-shortcuts.patch
+#patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/libxfce4ui-4.10.0-keyboard-shortcuts.patch
 
 set +e +o pipefail
